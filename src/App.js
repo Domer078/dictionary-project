7c74600b2aa12d5cac5c973afb86e057f14df774
Domer078 <khomotsomabala05@gmail.com>
2026-08-04 20:18:19 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dictionary Project</h1>
+      <a href="#" className="btn btn-primary">
+        Search
+      </a>
+    </div>
+  );
+}
+
+export default App;
