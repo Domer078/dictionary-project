2f9f7c5be64d0809344ad6b040d2426c7900c9f2
Domer078 <khomotsomabala05@gmail.com>
2026-08-04 20:30:14 +0200

    Removed button link

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ function App() {
   return (
     <div className="App">
       <h1>Dictionary Project</h1>
-      <a href="#" className="btn btn-primary">
-        Search
-      </a>
     </div>
   );
 }
