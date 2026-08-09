93ab8c625d9dfdd9921066d682cb3dfc99acc3de
Domer078 <khomotsomabala05@gmail.com>
2026-08-09 22:07:46 +0200

    Added a new apiUrl

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,31 +4,60 @@ import Results from "./Results";
 import "./Dictionary.CSS";
 
 export default function Dictionary() {
-  const [keyword, setKeyWord] = useState("");
-  const [results, setResults] = useState(null);
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function handleSearch(event) {
+  function search(event) {
     event.preventDefault();
-    let apiKey = "63340413at53cc6c6ba7a81a11oc3f05";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
 
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
-    setKeyWord(event.target.value);
+    setKeyword(event.target.value);
   }
 
   return (
     <div className="Dictionary">
-      <form onSubmit={handleSearch}>
-        <input type="search" onChange={handleKeywordChange}></input>
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
       </form>
       <Results results={results} />
     </div>
   );
 }
+
+// export default function Dictionary() {
+//   const [keyword, setKeyWord] = useState("");
+//   const [results, setResults] = useState(null);
+
+//   function handleResponse(response) {
+//     setResults(response.data[0]);
+//   }
+
+//   function handleSearch(event) {
+//     event.preventDefault();
+//     let apiKey = "63340413at53cc6c6ba7a81a11oc3f05";
+//     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+//     axios.get(apiUrl).then(handleResponse);
+//   }
+
+//   function handleKeywordChange(event) {
+//     setKeyWord(event.target.value);
+//   }
+
+//   return (
+//     <div className="Dictionary">
+//       <form onSubmit={handleSearch}>
+//         <input type="search" onChange={handleKeywordChange}></input>
+//       </form>
+//       <Results results={results} />
+//     </div>
+//   );
+// }
