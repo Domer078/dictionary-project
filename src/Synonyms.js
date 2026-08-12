a112b84b83bfc2be590e82f06a918d870518a580
Domer078 <khomotsomabala05@gmail.com>
2026-08-12 12:01:19 +0200

    Added a return statement

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,15 +3,13 @@ import React from "react";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <div className="Synonyms">
-        {props.synonyms.map(function (synonyms, index) {
-          return (
-            <span key={index}>
-              <li>{synonyms}</li>
-            </span>
-          );
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
         })}
-      </div>
+      </ul>
     );
+  } else {
+    return null;
   }
 }
