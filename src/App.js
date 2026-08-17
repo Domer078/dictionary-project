cbf8a23452933961a6be4e4869ec7de7e4978e3f
Domer078 <khomotsomabala05@gmail.com>
2026-08-17 02:16:57 +0200

    Added some CSS to my Footer

--- src/App.js
+++ src/App.js
@@ -9,7 +9,32 @@ export default function App() {
           <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Khomotso Mabala</small>
+          <small>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/Domer078"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Khomotso Mabala
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/Domer078/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://monumental-crepe-259aa0.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </small>
         </footer>
       </div>
     </div>
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -16,17 +16,3 @@ export default function Phonetic(props) {
     </div>
   );
 }
-
-// import React from "react";
-// import "./Phonetics.CSS";
-// export default function Phonetic(props) {
-//   console.log(props.phonetic);
-//   return (
-//     <div className="Phonetic">
-//       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-//         Listen
-//       </a>
-//       <span className="text">{props.phonetic.text}</span>
-//     </div>
-//   );
-// }
